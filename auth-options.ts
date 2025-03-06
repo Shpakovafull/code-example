fab725c52b4df0462fe232ae27addf7fc5942275
Shpakovafull <keddi726@gmail.com>
2025-03-06 21:52:11 +0200

    Add files via upload

---
+++ auth-options.ts
@@ -0,0 +1,126 @@
+import { AuthOptions } from 'next-auth';
+import CredentialsProvider from 'next-auth/providers/credentials';
+import GoogleProvider from 'next-auth/providers/google';
+
+import { prisma } from '@/prisma/prisma-client';
+import { compare, hashSync } from 'bcrypt';
+import { UserRole } from '@prisma/client';
+
+export const authOptions: AuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+    CredentialsProvider({
+      name: 'Credentials',
+      credentials: {
+        email: { label: 'Email', type: 'text' },
+        password: { label: 'Password', type: 'password' },
+      },
+      async authorize(credentials) {
+        if (!credentials) {
+          return null;
+        }
+
+        const values = {
+          email: credentials.email,
+        };
+
+        const findUser = await prisma.user.findFirst({
+          where: values,
+        });
+
+        if (!findUser) {
+          return null;
+        }
+
+        const isPasswordValid = await compare(credentials.password, findUser.password);
+
+        if (!isPasswordValid) {
+          return null;
+        }
+
+        if (!findUser.verified) {
+          return null;
+        }
+
+        return {
+          id: findUser.id,
+          email: findUser.email,
+          name: findUser.fullName,
+          role: findUser.role,
+        };
+      },
+    }),
+  ],
+  secret: process.env.NEXTAUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  callbacks: {
+    async signIn({ user, account }) {
+      try {
+        if (account?.provider === 'credentials') {
+          return true;
+        }
+
+        if (!user.email) {
+          return false;
+        }
+
+        const findUser = await prisma.user.findFirst({
+          where: {
+            email: user.email,
+          },
+        });
+
+        if (findUser) {
+          return true;
+        }
+
+        await prisma.user.create({
+          data: {
+            email: user.email,
+            fullName: user.name || 'User #' + user.id,
+            password: hashSync(user.id.toString(), 10),
+            verified: new Date(),
+          },
+        });
+
+        return true;
+      } catch (error) {
+        console.error('Error [SIGNIN]', error);
+        return false;
+      }
+    },
+    async jwt({ token }) {
+      if (!token.email) {
+        return token;
+      }
+
+      const findUser = await prisma.user.findFirst({
+        where: {
+          email: token.email,
+        },
+      });
+
+      if (findUser) {
+        token.id = String(findUser.id);
+        token.email = findUser.email;
+        token.fullName = findUser.fullName;
+        token.role = findUser.role;
+      }
+
+      return token;
+    },
+    session({ session, token }) {
+      if (session?.user) {
+        session.user.id = token.id;
+        session.user.role = token.role;
+      }
+
+      return session;
+    },
+  },
+};
